41808a14f104151c8f63c78401d8a659579a7aac
lavalo98 <65528180+lavalo98@users.noreply.github.com>
2022-02-03 14:26:33 -0900

    Added logic for flat file verification
    Removed hard coded username and password and added flat file verification.

--- Server.js
+++ Server.js
@@ -1,32 +1,46 @@
+const fs = require('fs');
+const readline = require('readline');
 const express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 const app = express();
+
+const readInterface = readline.createInterface({
+  input: fs.createReadStream('Logins.txt'),
+  output: process.stdout,
+  console: false
+});
   
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-var hardUsername = "admin";
-var hardPassword = "password";
+var listOfUsernames = []; // Created a list of usernames
+var listOfPasswords = []; // Created a list of passwords
     
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/LoginPage.html');
 });
-    
+
+// Takes the Logins.txt file and puts the username and password into their respective arrays
+readInterface.on('line', function(line) {
+  var words = line.split(" ");
+  listOfUsernames.push(words[0]);
+  listOfPasswords.push(words[1]);
+});
+
+// Called when the Submit button is clicked
 app.post('/', urlencodedParser, (req, res) => {
 
-  var usernameInput = req.body.username;
-  var passwordInput = req.body.password;
+  var usernameInput = req.body.username; // User inputted username
+  var passwordInput = req.body.password; // User inputted password
 
-  console.log('Username: ', usernameInput);
-  console.log('Password: ', passwordInput);
-
-  console.log('Username: ', hardUsername);
-  console.log('Password: ', hardPassword);
-    
-  if(usernameInput == hardUsername && passwordInput == hardPassword){
-    res.sendFile(__dirname + '/Success.html');
-  }else{
-    res.sendFile(__dirname + '/Failure.html');
+  for (let i = 0; i < listOfUsernames.length; i++) {
+    if(usernameInput == listOfUsernames[i] && passwordInput == listOfPasswords[i]){
+      res.sendFile(__dirname + '/Success.html');
+      return;
+    }
   }
+
+  res.sendFile(__dirname + '/Failure.html');
+ 
 });
     
 app.listen(80);

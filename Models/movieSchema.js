c7e81bc17622e3c492052966e2b846586c4b502e
Luis Avalo <65528180+lavalo98@users.noreply.github.com>
2022-03-13 22:44:54 -0800

    Adding Schema to add movies to DB

---
+++ Models/movieSchema.js
@@ -0,0 +1,41 @@
+const { Int32 } = require('bson');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const movieSchema = new mongoose.Schema({
+    movieName: {
+        type: String,
+        required: true
+    },
+    releaseYear: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    movieImageName: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: String,
+        required: true
+    },
+    runtime: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: Number,   
+        default: 0
+    },
+}, {timestamps: true});
+
+const Movie = mongoose.model('Movie', movieSchema);
+module.exports = Movie;

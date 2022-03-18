4bfb8d37dcee480c04a6a451fb68f1c820e91619
RootCellar <RootCellar9877@GMail.com>
2022-03-17 18:46:51 -0800

    rework dockertemp.sh

--- dockertemp.sh
+++ dockertemp.sh
@@ -1,3 +1,7 @@
-./dockerbuild.sh
-sudo docker rm -fv loginmodule
-sudo docker run --name loginmodule -p 80:80 -it --rm loginmodule
+#./dockerbuild.sh
+#sudo docker rm -fv loginmodule
+#sudo docker run --name loginmodule -p 80:80 -it --rm loginmodule
+
+docker-compose build
+docker-compose up
+docker-compose down

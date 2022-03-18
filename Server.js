0ba03c051527547a6b924a01d9d21a84411f1484
Luis Avalo <65528180+lavalo98@users.noreply.github.com>
2022-03-17 21:18:46 -0800

    Take video url from DB and display to user

--- Server.js
+++ Server.js
@@ -107,41 +107,51 @@ app.get('/', (req, res) => {
   //res.sendFile(__dirname + '/LoginPage.html');
 });
 
-app.get('/video', (req, res) => {
-  // Ensure there is a range given for the video
-  range = req.headers.range;
-  if (!range) {
-    range = 'bytes=0-';
-  }
-  
-  // get video stats 
-  const videoPath = "public/videos/game.mp4";
-  const videoSize = fs.statSync(videoPath).size;
-  
-  // Parse Range
-  // Example: "bytes=32324-"
-  const CHUNK_SIZE = 10 ** 6; // 1MB
-  const start = Number(range.replace(/\D/g, ""));
-  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-  
-  // Create headers
-  const contentLength = end - start + 1;
-  const headers = {
-    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-    "Accept-Ranges": "bytes",
-    "Content-Length": contentLength,
-    "Content-Type": "video/mp4",
-  };
-  
-  // HTTP Status 206 for Partial Content
-  res.writeHead(206, headers);
-  
-  // create video read stream for this particular chunk
-  const videoStream = fs.createReadStream(videoPath, { start, end });
-  
-  // Stream the video chunk to the client
-  videoStream.pipe(res);
-  
+app.get('/play-movie', (req, res) => {
+  var movie_query = decodeURI(req._parsedUrl.query);
+
+  Movie.find({"movieName" : movie_query})
+  .then((result) => {
+    console.log(result[0].movieFileName);
+
+    // Ensure there is a range given for the video
+    range = req.headers.range;
+    if (!range) {
+      range = 'bytes=0-';
+    }
+    
+    // get video stats 
+    const videoPath = "public/videos/" + result[0].movieFileName;
+    const videoSize = fs.statSync(videoPath).size;
+    
+    // Parse Range
+    // Example: "bytes=32324-"
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    
+    // Create headers
+    const contentLength = end - start + 1;
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": contentLength,
+      "Content-Type": "video/mp4",
+    };
+    
+    // HTTP Status 206 for Partial Content
+    res.writeHead(206, headers);
+    
+    // create video read stream for this particular chunk
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+    
+    // Stream the video chunk to the client
+    videoStream.pipe(res);
+
+  })
+  .catch((err) => {
+    console.log(err);
+  })  
 });
 
 app.get('/addmovie', (req, res) => {

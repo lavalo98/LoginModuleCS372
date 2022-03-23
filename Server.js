a2cb53d44d3b6b24f16e8931ac1d6047c9e176a2
RootCellar <RootCellar9877@GMail.com>
2022-03-23 09:59:51 -0800

    back-end for handling logout

--- Server.js
+++ Server.js
@@ -100,6 +100,16 @@ function shuffleArray(array) {
   return array;
 }
 
+app.get('/logout', (req, res) => {
+  if(req.session.loggedIn) {
+    req.session.loggedIn = false; // set loggedIn to false
+    return res.redirect("/"); // take the user back to the login screen
+  }
+  else {
+    return res.redirect("/home"); // the user will be redirected again with a warning message
+  }
+})
+
 app.get('/', (req, res) => {
 
   if(req.session.loggedIn) {

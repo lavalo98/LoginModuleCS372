c08007ba46d9ca3eef64633357e17bb480277b28
RootCellar <RootCellar9877@GMail.com>
2022-03-30 22:15:05 -0800

    fix bug where empty search would lead to a page without dashboard button for marketing manager

--- Routes/Search.js
+++ Routes/Search.js
@@ -10,21 +10,21 @@ router.get('/', (req, res) =>{
     if(!req.session.loggedIn || req.session.loggedIn == false) {
       return res.render("login", {alertShow: "show", header: "Access Denied", message: "You are not logged in!"});
     }
-  
+
     var search_query = req.query.search;
     var userRole = req.session.role;
     var movieNameArray = new Array();
     var movieImageArray = new Array();
     var releaseYearArray = new Array();
     var username = req.session.username;
-  
+
     if(search_query){
       var movieNameArray = new Array();
       var movieImageArray = new Array();
       var releaseYearArray = new Array();
       var username = req.session.username;
       const regex = new RegExp(escapeRegex(search_query), 'gi');
-  
+
       // Checks to see if any movie matches the regex request
       Movie.find({"movieName" : regex})
         .then((result) => {
@@ -41,7 +41,7 @@ router.get('/', (req, res) =>{
           return res.render("search", {movieNameArray, movieImageArray, releaseYearArray, username, search_query, amtOfResults : result.length, userRole});
       }).catch((err) => {console.log(err);})
     }else{
-      return res.render("search", {movieNameArray, movieImageArray, releaseYearArray, username, search_query, amtOfResults : 0});
+      return res.render("search", {movieNameArray, movieImageArray, releaseYearArray, username, search_query, amtOfResults : 0, userRole});
     }
 })
 
